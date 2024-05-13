0216904d01b625edbeb6dc705b32ffa7aa2c923b
Bryan1093 <bryan_loya_1995@hotmail.com>
2024-05-13 17:33:47 -0500

    Primer commit

---
+++ src/main/java/ec/edu/uce/test/ProyectoPrueba/ProyectoPruebaApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.uce.test.ProyectoPrueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPruebaApplication.class, args);
+	}
+
+}
